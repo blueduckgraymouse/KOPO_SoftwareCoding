8171e17b107a7be462ba135c39d315a3d1d2e872
KOPO_35 <hyunduok@gmail.com>
2022-04-04 17:43:04 +0900

    프로젝트 2번문제 수정

--- eclipse-workspace/KOPO_javaBase/src/pro/Quiz02.java
+++ eclipse-workspace/KOPO_javaBase/src/pro/Quiz02.java
@@ -12,10 +12,9 @@ public class Quiz02 {
 	public static void main(String[] args) {
 //	      String inputString = "abcdaefgh";
 //	      String targetString = "gcdafhaeb";
-	      String inputString = "lorl Hewlod";
+	      String inputString = "lorl HeWlod";
 	      String targetString = "Hello World";
 
-	      
 	      int result = RearrangementPhonemes(inputString, targetString);
 	      
 	      System.out.println("\nTimes : " + result);
@@ -42,12 +41,12 @@ public class Quiz02 {
 	      /* PROCESS */
 	      // Divide 'inputString' by alphabets with a common sequence
 	      String[] iString = new String[4];
-	      iString[0] = inputString.substring(0, IndexOfFirstAlp);                        // Before the 'IndexOfFirstAlp'                                                => "abcdeaef"
-	      iString[1] = commonArrangement;                                          // Alphabets with a common sequence between input string and target string from 'IndexOfFirstAlp'   => "g"
-	                                                                     // ex) abcde / bcaed => a'bc'de / 'bc'aed
+	      iString[0] = inputString.substring(0, IndexOfFirstAlp);           // Before the 'IndexOfFirstAlp'                                                => "abcdeaef"
+	      iString[1] = commonArrangement;                                   // Alphabets with a common sequence between input string and target string from 'IndexOfFirstAlp'   => "g"
+	                                                                     	// ex) abcde / bcaed => a'bc'de / 'bc'aed
 	      iString[2] = inputString.substring(IndexOfFirstAlp + commonArrangement.length());   // After the end of the alphabets with a common sequence, 'commonArrangement'            => "h"
-	      iString[3] = "";                                                   // A space where alphabets will be saved after each actions of rearrangement
-	                                                                     // ex) a / bc / de / _ 
+	      iString[3] = "";                                                  // A space where alphabets will be saved after each actions of rearrangement
+	      																	// ex) a / bc / de / _ 
 	      // Rearrange and Print the steps
 	      System.out.println("\n* Before action : \t" + inputString);
 	      int commonArrangementSize = iString[1].length();
@@ -55,13 +54,15 @@ public class Quiz02 {
 	      for (int i = 0 ; i < inputString.length() - commonArrangementSize ; i++) {         // Loop time : inputString length - 'commonArrangement' length
 	         // Check a target Alphabet to rearrange
 	         String targetAlphabet = targetString.substring(iString[1].length() + i, iString[1].length() + i + 1);
-	         System.out.println(" -> rearrange \'" + targetAlphabet + "\'");
+	         System.out.print(" -> rearrange \'" + targetAlphabet + "\' , index : ");
 	         // Check the target alphabet is before the 'IndexOfFirstAlp' or after the end of the alphabets with a common sequence AND Rearrange the alphabets in iString[0] or iString[2] to iString[3]
 	         if (iString[0].contains(targetAlphabet)) {
+	        	 System.out.println(iString[0].indexOf(targetAlphabet));
 	            iString[3] = iString[3] + targetAlphabet;            
 	            iString[0] = iString[0].replaceFirst(targetAlphabet, "");
 	            count++;
 	         } else if (iString[2].contains(targetAlphabet)) {
+	        	 System.out.println(iString[0].length() + iString[0].length() + iString[2].indexOf(targetAlphabet));
 	            iString[3] = iString[3] + targetAlphabet;            
 	            iString[2] = iString[2].replaceFirst(targetAlphabet, "");
 	            count++;
