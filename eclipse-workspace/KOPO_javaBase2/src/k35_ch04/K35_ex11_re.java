4e867cfa2a1ac7602fa998983de639b6ef98bc9a
KOPO_35 <hyunduok@gmail.com>
2022-04-18 19:18:10 +0900

    ch04 ex11_re refactoring

---
+++ eclipse-workspace/KOPO_javaBase2/src/k35_ch04/K35_ex11_re.java
@@ -0,0 +1,50 @@
+package k35_ch04;
+
+/** 소프트웨어 코딩 심화 4강 - p23 실습
+ * 
+ *  숫자 한글로 읽기 고급 - 오류 수정
+ * 
+ * @author KOPO35
+ *
+ */
+public class K35_ex11_re {
+	public static void main(String[] args) {
+		// 변수 선언
+		int k35_iNumVal = 1000000000;												// 한글로 읽을 숫자를 int형 변수 iNumVal에 초기화
+		String k35_sNumVal = String.valueOf(k35_iNumVal);							// iNumVal에 저장된 한글로 읽을 숫자를 String형 변수 sNumVal에 초기화
+		String k35_sNumVoice = "";													// 한글로 읽은 문자열이 저장될 String형 변수 sNumVoice 선언
+		String[] k35_units = {"영", "일", "이", "삼", "사", "오", "육", "칠", "팔", "구"};	// 0~9까지 숫자 한글을 배열 units으로 초기화
+		String[] k35_unitX = {"", "십", "백", "천", "만", "십", "백", "천", "억", "십"};	// 자리 수에 따른 한글 표현을 배열 unitX에 초기화
+		int k35_i = 0;																// 한글로 왼쪽 숫자부터 읽을 때 현재 읽고 있는 자리수를 나타내는 변수 i를 0으로 초기화 
+		int k35_j = k35_sNumVal.length() - 1;										// 위의 i자리에서 현재 자리의 숫자가 나타내는 단위를 확인하기 위해서 숫자의 총 길이를 변수 j에 초기화
+		
+		// 한글로 읽어야 하는 숫자와 총 자리수 출력
+		System.out.printf("==> %s [%d자리]\n", k35_sNumVal, k35_sNumVal.length());
+		
+		// 왼쪽 자리수부터 오른쪽방향으로 한글로 읽어 sNumVoice에 저장하는 반복문
+		while(true) {																// 무한 반복
+			if (k35_i >= k35_sNumVal.length())										// 현재까지 읽은 자리 수의 개수가 읽어야 하는 숫자의 총 자리수 개수와 같거나 커지면 
+				break;																//   다 읽은 것이므로 반복문 종료
+			
+			String k35_digit = k35_sNumVal.substring(k35_i, k35_i + 1);				// 문자열로 저장된 읽어야 하는 숫자로 부터 지금 loop에서 읽어야 하는 자리의 숫자 한 개 추출하여 변수 digit에 저장
+			
+			System.out.printf("%s[%s]",												// 지금 loop에서 읽어야 하는 자리의 숫자 한 개(digit)의 숫자 값과 한글 값 출력
+					k35_digit,
+					k35_units[Integer.parseInt(k35_digit)]);
+			
+			if (k35_digit.equals("0")) {											// 지금 loop에서 읽어야 하는 자리의 숫자 한 개(digit)가 0이면
+				if ((k35_unitX[k35_j].equals("만") || k35_unitX[k35_j].equals("억")) //   '만'의 자리나 '억'의 자리 일 경우 0이어도 이전에 출력된 일십(만)~구백(만) 혹은 일십(억)~구백(억)을 위해서 읽어줘야 한다.
+						&& !k35_sNumVoice.substring(k35_sNumVoice.length()-1).equals("억")) { // 만~백만 단위에 해당하는 숫자가 비였을 때 '만'이 출력되는 것을 방지하기 위해, '만'이 찍히려고 할 때 sNumVoice의 마지막 글자가 억이면 안 찍히도록 설정
+					k35_sNumVoice = k35_sNumVoice + "" + k35_unitX[k35_j];			//   이전 자리수까지 한글로 읽어서 저장된 문자열에 이어서 현재 자리의 숫자한글 저장
+				}
+			} else {																// 지금 loop에서 읽어야 하는 자리의 숫자 한 개(digit)가 0이 아니면
+				k35_sNumVoice = k35_sNumVoice + k35_units[Integer.parseInt(k35_digit)] + k35_unitX[k35_j];	// 이전 자리수까지 한글로 읽어서 저장된 문자열에  이어서 현재 자리의 숫자한글 저장
+			}
+			k35_i++;																// i를 증가시켜 다음 루프에서 현재 자리수의 하나 오른쪽 자리를 읽도록 설정 
+			k35_j--;																// j를 감소시켜 다음 루프에서 읽는 자리수의 단위를 감소시킴
+		}
+		
+		// 한글로 읽은 숫자 문자열 출력
+		System.out.printf("\n %s[%s]\n", k35_sNumVal, k35_sNumVoice);
+	}
+}
